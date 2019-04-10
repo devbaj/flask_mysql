8ed03a13cdd6b1957c1c07ccc7ddb21e72b88171
Brett Jory <bjory7@gmail.com>
2019-04-10 13:36:42 -0700

    Display Received Messages implemented. Message Verification (>5) implemented. Dashboard Access Restriction implemented.

--- private_wall/server.py
+++ private_wall/server.py
@@ -81,9 +81,6 @@ def login():
         if bcrypt.check_password_hash(result[0]["pw_hash"], request.form["pw"]):
             session["userid"] = result[0]["id"]
             id = session["userid"]
-            print("*"*30)
-            print(id)
-            print("*"*30)
             return redirect(f"/user/{id}")
         else:
             flash(u"You could not be logged in.", "error")
@@ -104,6 +101,9 @@ def logout():
 ######################
 @app.route("/user/<id>")
 def user_home(id):
+    if not "userid" in session:
+        flash(u"You must be logged in as a registered user to access your wall.", "invalid")
+        return redirect("/")
     # QUERY CURRENT USER NAME TO DISPLAY WELCOME TEXT
     mysql = connectToMySQL("private_wall")
     query = "SELECT first_name FROM users WHERE id = %(id)s;"
@@ -114,35 +114,47 @@ def user_home(id):
     
     # QUERY LIST OF USERS TO DISPLAY CORRESPONDING SEND MESSAGE FORMS
     mysql2 = connectToMySQL("private_wall")
-    query = "SELECT id as recipient_id, first_name, last_name FROM users WHERE id != %(id)s;"
-    data = {
+    query2 = "SELECT id as recipient_id, first_name, last_name FROM users WHERE id != %(id)s;"
+    data2 = {
         "id": session["userid"]
     }
-    users_list = mysql2.query_db(query, data)
+    users_list = mysql2.query_db(query2, data2)
     # print("*"*30)
     # print(users_list)
     # print("*"*30)
     
-    # QUERY LIST OF MESSAGES WHERE CURRENT USER IS THE RECIPIENT
+    # QUERY LIST OF MESSAGES WHERE CURRENT USER IS THE RECIPIENT FOR DISPLAY
     mysql3 = connectToMySQL("private_wall")
-    query = "SELECT * FROM messages WHERE recipient_id = %(id)s"
-    data = {
+    query3 = "SELECT users.id, users.first_name AS recipient_name, users2.first_name AS sender_name, messages.content, messages.sender_id, messages.created_at FROM users INNER JOIN messages ON users.id = messages.recipient_id AND messages.recipient_id = %(id)s INNER JOIN users AS users2 ON messages.sender_id = users2.id;"
+    data3 = {
         "id": session["userid"]
     }
+    msg_list = mysql3.query_db(query3, data3)
     
-    return render_template("userhome.html", fname_html = user_fn[0]["first_name"], users_list_html = users_list)
+    # QUERY TOTAL NUMBER OF MESSAGES SENT BY THIS USER FOR DISPLAY
+    mysql4 = connectToMySQL("private_wall")
+    query4 = "SELECT COUNT(*) AS count FROM messages WHERE sender_id = %(id)s"
+    data4 = {
+        "id": session["userid"]
+    }
+    sent_count = mysql4.query_db(query4, data4)
+    print("*"*30)
+    print(sent_count)
+    print("*"*30)
+    
+    return render_template("userhome.html", fname_html = user_fn[0]["first_name"], users_list_html = users_list, msg_list_html = msg_list, sent_count_html = sent_count[0])
 
 ######################
 # SEND MESSAGE
 ######################
 @app.route("/send-message/<recipient_id>", methods=["POST"])
 def send_msg(recipient_id):
-    if len(request.form["msg-content"])  < 0:
-        flash(u"Message is empty. Message was not sent.", "invalid")
+    if len(request.form["msg-content"])  < 5:
+        flash(u"Message not sent; messages must be at least 5 characters long.", "invalid")
     else:
         mysql = connectToMySQL("private_wall")
         query = "INSERT INTO messages(sender_id, recipient_id, content, created_at, updated_at) VALUES (%(sender_id)s, %(recipient_id)s, %(content)s, NOW(), NOW());"
-        data= {
+        data = {
             "sender_id": session["userid"],
             "recipient_id": recipient_id,
             "content": request.form["msg-content"]
